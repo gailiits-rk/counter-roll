f0bf9fd477754ca11b68ad4c005112e071083cea
Vaffeled <gailisrobors@gmail.com>
2026-04-24 20:45:52 +0300

    added login, started work on sign up

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 from pathlib import Path
+from werkzeug.security import check_password_hash
 
 def get_db_connection():
     db = Path(__file__).parent / "counter-roll.db"
@@ -10,12 +11,39 @@ def get_db_connection():
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        conn = get_db_connection()
+
+        # Get user by email
+        user = conn.execute(
+            "SELECT * FROM Accounts WHERE email = ? AND password = ?",
+            (email,password)
+        ).fetchone()
+
+        conn.close()
+        if user == None:
+            return render_template("index.html", first = False)   
+        else:
+            return redirect(url_for("opening"))
+    
+    return render_template("index.html", first = True)
+
+@app.route("/opening")
+def opening():
     conn = get_db_connection()
     cases = conn.execute("SELECT * FROM cases").fetchall()
     conn.close()
-    return render_template("index.html", cases=cases)
+    return render_template("opening-page.html", cases=cases)
+
+@app.route("/sign-up")
+def sign_up():
+    return render_template("sign_up.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
